8a3a777c76dd618cce73f0d4523d4422f05add79
Valeriia <topolka311@gmail.com>
2024-07-11 18:01:04 +0300

    change homework30

--- homework30/task1.Swapi/src/core/thunks/getInfoThunk.js
+++ homework30/task1.Swapi/src/core/thunks/getInfoThunk.js
@@ -1,5 +1,19 @@
-export function logApiInfo(response) {
-    return (dispatch) => {
-         console.log(response ? response : 'Api was called')
-    }
+import swapiSlice from "../slice/swapiSlice.js";
+
+
+export const requestApiInfo = (formData) => async (dispatch, state) => {
+    formData.preventDefault();
+
+    dispatch(swapiSlice.actions.requestedInfo())
+
+    const requestingApiRoute = formData.target.apiRoute.value;
+    const params = requestingApiRoute.toString().split('/')
+
+    const swapiResponse = await fetch('https://swapi.dev/api/' + requestingApiRoute);
+    const data = await swapiResponse.json();
+
+    dispatch(swapiSlice.actions.setParams(params))
+    dispatch(swapiSlice.actions.setInfo(data))
+
+    dispatch(swapiSlice.actions.respondedInfo())
 }
--- homework30/task1.Swapi/src/ui/components/Apiform.jsx
+++ homework30/task1.Swapi/src/ui/components/Apiform.jsx
@@ -1,28 +1,12 @@
 import {useDispatch, useSelector} from "react-redux";
-import {logApiInfo} from "../../core/thunks/getInfoThunk.js";
+import {requestApiInfo} from "../../core/thunks/getInfoThunk.js";
 import selectors from "../../core/selectors.js";
-import swapiSlice from "../../core/slice/swapiSlice.js";
 
 function Apiform() {
     const isLoading = useSelector(selectors.isLoading)
     const dispatch = useDispatch()
-    const getApiInfo = async (formData) => {
-        formData.preventDefault();
-
-        dispatch(swapiSlice.actions.requestedInfo())
-
-        const requestingApiRoute = formData.target.apiRoute.value;
-        const params = requestingApiRoute.toString().split('/')
-        console.log(params)
-
-        const swapiResponse = await fetch('https://swapi.dev/api/'+requestingApiRoute);
-        const data = await swapiResponse.json();
-
-        dispatch(swapiSlice.actions.setParams(params))
-        dispatch(swapiSlice.actions.setInfo(data))
-        dispatch(logApiInfo(data))
-
-        dispatch(swapiSlice.actions.respondedInfo())
+    const getApiInfo = (formData) => {
+        dispatch(requestApiInfo(formData))
     }
     return (
         <form action='#' className="mb-3 apiform input-group" onSubmit={getApiInfo}>
